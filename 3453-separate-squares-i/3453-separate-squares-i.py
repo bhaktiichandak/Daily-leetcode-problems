82aad12fde57f65475b89df3ace1aac098a4b67d
Bhakti Chandak <bhaktichandak04@gmail.com>
2026-01-13 21:54:22 +0530

    Time: 254 ms (97.07%), Space: 51.6 MB (5.28%) - LeetHub

--- 3453-separate-squares-i/3453-separate-squares-i.py
+++ 3453-separate-squares-i/3453-separate-squares-i.py
@@ -1,22 +1,16 @@
 class Solution:
-    def separateSquares(self, squares):
-        def area_diff(h):
-            below = 0
-            total = 0
-            for _, y, l in squares:
-                total += l * l
-                if h > y:
-                    below += min(h - y, l) * l
-            return below - (total - below)
-
-        low = min(y for _, y, _ in squares)
-        high = max(y + l for _, y, l in squares)
-
-        for _ in range(60):  # enough for 1e-5 precision
-            mid = (low + high) / 2
-            if area_diff(mid) >= 0:
-                high = mid
-            else:
-                low = mid
-
-        return low
+    def separateSquares(self, squares: List[List[int]]) -> float:
+        total_area = 0
+        diff = defaultdict(int)
+        for _, y, l in squares:
+            total_area += l * l
+            diff[y] += l
+            diff[y + l] -= l
+        
+        area = 0
+        s = 0
+        for y, y2 in pairwise(sorted(diff)):
+            s += diff[y]
+            area += s * (y2 - y)
+            if area * 2 >= total_area:
+                return y2 - (area * 2 - total_area) / (s * 2)
