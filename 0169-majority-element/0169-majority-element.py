d12128beb5d28edf6612296cbeabb0203c82a1f8
Bhakti Chandak <bhaktichandak04@gmail.com>
2026-03-27 16:15:42 +0530

    Time: 6 ms (61.27%), Space: 21.2 MB (73.48%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,17 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        el = 0
+        n = len(nums)
+        cnt = 0
+        for num in nums:
+            if cnt == 0 :
+                cnt = 1
+                el = num
+            elif el == num:
+                cnt += 1
+            else:
+                cnt -= 1
+        cnt1 = nums.count(el)
+        if cnt1 > (n//2):
+            return el
+        return -1
