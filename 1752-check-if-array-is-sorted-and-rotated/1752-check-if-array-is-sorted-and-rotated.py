4f3cc8bf6eb204e6bbb4d76ca0f941bc9d91f940
Bhakti Chandak <bhaktichandak04@gmail.com>
2026-05-23 16:07:05 +0530

    Time: 0 ms (100%), Space: 19.4 MB (16.1%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -4,7 +4,7 @@ class Solution:
         n = len(nums)
 
         for i in range(n):
-            if nums[i] > nums[(i + 1) % n]:
+            if nums[i] > nums[(i + 1) % n]: #used modulo so that after finishing the    array we go back to the first element rather than index bound 
                 count += 1
 
         return count <= 1
