1bdaaf99006f43b0d0e1814967d500428d773d1d
Bhakti Chandak <bhaktichandak04@gmail.com>
2026-06-18 12:31:18 +0530

    Time: 0 ms (100%), Space: 19.2 MB (67.21%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        hashMap ={ ")" : "(" , "]" : "[" , "}" : "{" }
+        for ch in s:
+            if ch in hashMap:
+                if stack and stack[-1] == hashMap[ch]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(ch)
+        return len(stack) == 0
