5522cdb38dac97eae7796bd439ad015c74033378
Bhakti Chandak <bhaktichandak04@gmail.com>
2026-09-05 15:35:34 +0530

    Time: 233 ms (99.94%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,7 @@
-SELECT 
-    (SELECT DISTINCT salary 
-     FROM Employee 
-     ORDER BY salary DESC 
-     LIMIT 1 OFFSET 1) AS SecondHighestSalary;
+WITH RankedSalaries AS (
+    SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) as rnk
+    FROM Employee
+)
+SELECT MAX(salary) AS SecondHighestSalary 
+FROM RankedSalaries 
+WHERE rnk = 2;
